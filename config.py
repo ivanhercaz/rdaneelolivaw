cdaf9ed77ad30f5574aa4b2cd607f2460507e732
ivanhercaz <ivan@ivanhercaz.com>
2019-06-04 01:58:59 +0100

    añadidas constantes necesarias para Github

--- config.py
+++ config.py
@@ -3,10 +3,11 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-envPath = join(dirname(__file__), '.env')
+envPath = join(dirname(__file__), ".env")
 load_dotenv(envPath)
 
-# Accessing variables.
-TOKEN = os.getenv('DANEEL_TOKEN')
-GITHUB_USER = os.getenv('DANEEL_GITHUB_USER')
-GITHUB_PASS = os.getenv('DANEEL_GITHUB_PASS')
+# Environment variables.
+TOKEN = os.getenv("DANEEL_TOKEN")
+GITHUB_USER = os.getenv("DANEEL_GITHUB_USER")
+GITHUB_PASS = os.getenv("DANEEL_GITHUB_PASS")
+GITHUB_REPO_ISSUES = os.getenv("DANEEL_GITHUB_REPO_ISSUES")

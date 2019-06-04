8db7348253b213672cf0ad660423cb9e6b96e0d1
ivanhercaz <ivan@ivanhercaz.com>
2019-06-04 01:58:06 +0100

    añadido función para crear issues
    De esta manera Daneel puede crear issues a partir del comando /proponer

--- bot.py
+++ bot.py
@@ -1,6 +1,14 @@
+from github import Github
 from telegram.ext import Updater, CommandHandler
 
+import logging
+
 import config
+
+github = Github(config.GITHUB_USER, config.GITHUB_PASS)
+repo = github.get_repo(config.GITHUB_REPO_ISSUES)
+
+logger = logging.getLogger("bot")
 
 
 def hello(bot, update):
@@ -9,8 +17,25 @@ def hello(bot, update):
 
 
 def proposeAwesomeList(bot, update):
-    update.message.reply_text(
-        'Recurso propuesto en una __issue__ en el repositorio de makersGC/awesome-micropython')
+    logger.info("Issue created")
+    resource = update.message.text.replace("/proponer ", "")
+    resourceTitle = resource.split("]")[0].replace("[", "")
+    user = update.message.from_user.username
+
+    labels = [repo.get_label("enhancement"),
+              repo.get_label("telegram")]
+    issue = f"{user} propone añadir el siguiente recurso:\n- {resource}"
+    print(issue)
+
+    repo.create_issue(
+        title=f"Recurso propuesto: {resourceTitle}",
+        body=issue,
+        labels=labels
+    )
+
+    update.message.reply_markdown(
+        f'Recurso `{resourceTitle}` propuesto en una en el repositorio de `makersGC/awesome-micropython`',
+        quote=True)
 
 
 updater = Updater(config.TOKEN)
